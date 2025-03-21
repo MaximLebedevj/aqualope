b0de6b11006ad6f4fbf3ca3a73eaf2d29d3b8ad7
maximlebedevj <maximlebedevj@gmail.com>
2025-03-21 13:14:32 +1000

    short stand JSON titles

--- stand/main.py
+++ stand/main.py
@@ -4,23 +4,22 @@ import random
 import json
 
 
-broker = "broker.emqx.io"
+broker = "test.mosquitto.org"
 port = 1883
 topic = "topic/water_quality"
 
 
 def generate_sensor_data():
     data = {
-        "temperature": round(random.uniform(18.0, 30.0), 2),
-        "oxygen_saturation": round(random.uniform(80, 100), 2),
-        "pH": round(random.uniform(6.5, 8.5), 2),
-        "orp": round(random.uniform(200, 800), 2),
-        "salinity": round(random.uniform(0.1, 5.0), 2),
-        "water_level": round(random.uniform(0, 100), 2),
-        "turbidity": round(random.uniform(0, 100), 2),
-        "ammonia": round(random.uniform(0, 5.0), 2),
-        "nitrites": round(random.uniform(0, 1.0), 2),
-        "timestamp": int(time.time())
+        "te": round(random.uniform(18.0, 30.0), 2),
+        "ox": round(random.uniform(80, 100), 2),
+        "p": round(random.uniform(6.5, 8.5), 2),
+        "or": round(random.uniform(200, 800), 2),
+        "s": round(random.uniform(0.1, 5.0), 2),
+        "w": round(random.uniform(0, 100), 2),
+        "tu": round(random.uniform(0, 100), 2),
+        "a": round(random.uniform(0, 5.0), 2),
+        "n": round(random.uniform(0, 1.0), 2)
     }
     return data
 
